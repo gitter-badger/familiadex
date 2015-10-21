ea7e07b7fc93c61b8b3e48dcd2b66ec29dff547d
Damian Trojnar <trojnar.damian@gmail.com>
2015-10-21 12:06:36 +0200

    Change name to Familiada

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -3,13 +3,13 @@ use Mix.Config
 # In this file, we keep production configuration that
 # you likely want to automate and keep it away from
 # your version control system.
-config :familiadex, Familiadex.Endpoint,
+config :familiada, Familiada.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :familiadex, Familiadex.Repo,
+config :familiada, Familiada.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("DATABASE_USERNAME"),
   password: System.get_env("DATABASE_PASSWORD"),
-  database: "familiadex_prod",
+  database: "familiada_prod",
   size: 20 # The amount of database connections in the pool
